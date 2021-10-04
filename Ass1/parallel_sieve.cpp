71dc4c8cd676a973344a20b2b0eb6857ee3a2af7
geo-j <g.juglan@tutanota.com>
2021-10-04 10:19:11 +0200

    first draft?

--- Ass1/parallel_sieve.cpp
+++ Ass1/parallel_sieve.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+int x = 2;
+
 bool isPrime(int n){
     if (n < 2){
         return false;
@@ -21,43 +23,65 @@ bool isPrime(int n){
 }
 
 int main(){
-    int n, x = 2;
+    int n;
     cin >> n;
 
     bulk::thread::environment env;
-    env.spawn(env.available_processors(), [&n, &x](auto& world) {
+    env.spawn(env.available_processors(), [&n](auto& world) {
         // init local processors
         auto pid = world.rank(); // local processor ID
         auto p = world.active_processors();
 
+
         // create cyclically partitioned array
         auto cp = bulk::cyclic_partitioning<1>({n}, {p});
         auto cyclic_local_size = cp.local_count(world.rank());
-        auto all_numbers = bulk::coarray<int>(world, cyclic_local_size);
+        auto primes = bulk::coarray<bool>(world, cyclic_local_size);
 
         // create blockly partitioned boolean array
-        auto bp = bulk::block_partitioning<1>({n}, {p});
-        auto block_local_size = bp.local_count(world.rank());
-        auto primes = bulk::coarray<bool>(world, block_local_size); // boolean array holding 1 if a number is prime, 0 otherwise
+        // auto bp = bulk::block_partitioning<1>({n}, {p});
+        // auto block_local_size = bp.local_count(world.rank());
+        // auto primes = bulk::coarray<bool>(world, block_local_size); // boolean array holding 1 if a number is prime, 0 otherwise
 
         // fill all numbers array with numbers from 2 to n
-        world.log("cyclic size %d", cyclic_local_size);
-        for (int i = 0; i < cyclic_local_size; i ++){
-            world.log("processor %d, index %d", pid, i);
-            all_numbers(pid)[i] = x;
-            x ++;
-        }
+        // world.log("cyclic size %d", cyclic_local_size);
+        // for (int i = 0; i < cyclic_local_size; i ++){
+        //     world.log("processor %d, index %d, number %d", pid, i, x);
+        //     // all_numbers(pid)[i] = x(world.next_rank()).get().value();
+        //     // x ++;
+        //     world.sync();
+        // }
 
-        world.log("block size %d", block_local_size);
-        for (int i = 0; i < block_local_size; i ++){
-            world.log("processor %d, index %d", pid, i);
-            primes(pid)[i] = 1;
+        // world.sync();
+        // world.log("block size %d", block_local_size);
+        for (int i = 0; i < cyclic_local_size; i ++){
+            world.log("processor %d, index %d, number %d", pid, i, pid + i * p + 1);
+            primes[i] = true;
         }
 
         world.sync();
 
-        for (auto x : primes){
-            world.log("%d", x);
+        for (int i = 0; i < cyclic_local_size; i ++){
+            int current = pid + i * p + 1;
+            world.log("processor %d, current %d", pid, current);
+            if (primes[i] || isPrime(current)){
+                world.log("\tprocessor %d, prime found %d", pid, current);
+                for (int j = i + 1; j < cyclic_local_size; j ++) {
+                    world.log("\tprocessor %d, trying %d for division", pid, pid + j * p + 1);
+                    if ((pid + j * p + 1) % current == 0){
+                        world.log("\tmark processor %d, index %d", pid * j, j);
+                        primes[j] = false;
+                    }
+                }
+            } else {
+                primes[i] = false;
+            }
+        }
+
+        world.sync();
+
+        for (int i = 0; i < cyclic_local_size; i ++){
+            world.log("number %d, prime? %d", pid + i * p + 1, primes[i]);
         }
     });
 }
