68469c929f065739df981d0bdb25d8225e1d5278
geo-j <g.juglan@tutanota.com>
2021-12-07 14:48:38 +0100

    WORKS

--- Ass2/saw.cpp
+++ Ass2/saw.cpp
@@ -10,102 +10,138 @@
 
 using namespace std;
 
-const int SYNC_TIME = 100;
-
 struct work {
-    long long int w;
+    long long int v;
     vector<int> visited;
-    long long int count;
-    long long int N;
+    // long long int count;
+    long long int cur_path_length;
 };
 
-void saw(bulk::world &world, long long int n, long long int N, long long int v, vector<vector<int>> A, long long int i, vector<int> visited, vector<long long int> walk, long long int &count, long long int p, long long int pid, vector<work> &work_stack, vector<long long int> &final_nodes, long long int cur_N) {
+// OPTIMISATIONS:
+// TODO: use an adjacency list instead of a matrix
+// TODO: make visited an integer instead of a  bool array
+// TODO: make redistribute work cyclically but not by going trough everything (do clever divisions)
+
+// void saw(bulk::world &world, long long int n, long long int N, long long int v, vector<vector<int>> A, long long int i, vector<int> visited, vector<long long int> walk, long long int &count, long long int p, long long int pid, vector<work> &work_stack, long long int cur_N) {
+void saw(bulk::world &world, long long int n, long long int N, long long int v, vector<vector<int>> A, long long int i, vector<int> visited, long long int &count, long long int p, long long int pid, vector<work> &work_stack) {
+    
     /*
     Input: the world, the time since last sync
     the int n, representing the size of graph
     the int N, representing the length of the paths we are interested in
     the int v, representing the node we are at
+    the int i, representing the current path length // change to cur_path_length
     the matrix A, which is the adjecency matrix of our graph
-    the path length so far 
-    the vector of all nodes we have visited so far vector<int> visited, 
-    the vector<long int> walk, 
+    the vector of all nodes we have visited so far vector<int> visited,  
     long int &count, how many walks has this processor counted. , 
     long int p, 
     long int pid
 
-    a boolean top which indicates whether this process is the topmost called by the current processor. 
+    Effects:
+        first checks whether we have visited the vector, if not we don't do anything. 
+            if the path length is the maximal length we increment the count
+            otherwise, we add all neighbours of v to the workstack as if we were travelling there. 
     */
 
-    world.log("processor %d finding all paths of length %d starting at length %d and node %d", pid, cur_N, i, v);
-    world.log("trying node %d, visited? %d", v, visited.at(v));
+    // world.log("processor %d finding all paths of length %d starting at length %d and node %d", pid, cur_N, i, v);
+    // world.log("trying node %d, visited? %d", v, visited.at(v));
     if (!visited.at(v)) {
+        // world.log("SAW called from processor %d at node %d",  pid, v);
+            /*
+            //First we check whether we are done
+            if (cur_path_length == N ){
+                count ++ ;
+            }
+            else {
+                visited [v] = true 
+                for  w neighbours of v
+                add ( w, cur_paht_length +1, visited) to workstack 
+            }
+            */
+
         // world.log("i = %d, cur_N = %d, N = %d", i, cur_N, N);
-         if (i == cur_N) {
+         if (i == N) {
             // for (long int j = 0; j < walk.size(); j ++) {
             //     world.log("%d, ", walk.at(j));
             // }
             count ++;
+            world.log("processor %d finished at ending node = %d, count = %d", pid, v, count);
+            // for (long long int i = 0; i < visited.size(); i ++) {
+            //         if (visited.at(i))
+            //             world.log("%d,", i);
+            //     }
+            // world.log("%d", v);
 
-            world.log("processor %d finished at starting node = %d, count = %d, path length = %d", pid, v, count, i);
-            for (long long int i = 0; i < visited.size(); i ++) {
-                    if (visited.at(i))
-                        world.log("%d,", i);
-                }
-            world.log("%d", v);
-
-            if (i < N) {
-                work_stack.push_back(work(v, vector<int>(visited), count, cur_N + 1));
-                // final_nodes.push_back(v);
-                // work.visited = vector<int>(visited);
-            }
+            // if (i < N) {
+            //     work_stack.push_back(work(v, vector<int>(visited), count, cur_N + 1));
+            //     // final_nodes.push_back(v);
+            //     // work.visited = vector<int>(visited);
+            // }
                 
         }
         else {
-            world.log("\tvisiting node %d", v);
+            // world.log("\tvisiting node %d", v);
             visited[v] = true;
             for (long int w = 0; w < n; w ++) {
                 if (A[v][w]) {
-                    // work_stack.push_back(work(w, vector<int>(visited), count, cur_N ));
-                    walk.push_back(w);
-                    saw(world, n, N, w, A, i + 1, visited, walk, count, p, pid, work_stack, final_nodes, cur_N);
-                    walk.pop_back();
+                    // We should take care to send a copy of visited as it will be different the next time we call it. 
+                    work_stack.push_back(work(w, vector<int>(visited), i + 1));
+                    // walk.push_back(w);
+                    // saw(world, n, N, w, A, i + 1, visited, walk, count, p, pid, work_stack, cur_N);
+                    // walk.pop_back();
                 }
             }
-            visited[v] = false;
-            world.log("\tunvisiting node %d", v);
+            // visited[v] = false;
+            // world.log("\tunvisiting node %d", v);
 
         }
     }
 }
 
-void send_dones(bulk::world &world, long long int p, long long int pid, int done, bulk::queue<int> &send_done) {
-    // auto send_done = bulk::queue<int>(world);
+// void send_dones(bulk::world &world, long long int p, long long int pid, int done, bulk::queue<int> &send_done) {
+//     // auto send_done = bulk::queue<int>(world);
 
+//     for (long long int i = 0; i < p; i ++) {
+//             send_done(i).send(done);
+//     }
+// }
+
+void send_work_stack_lengths(bulk::world &world, long long int p, long long int pid, int work_stack_length, bulk::queue<long long int, int> &send_work_stack_length) {
     for (long long int i = 0; i < p; i ++) {
-            send_done(i).send(done);
+        send_work_stack_length(i).send(pid, work_stack_length);
     }
 }
 
-void send_works(bulk::world &world, long long int p, long long int pid, vector<work> work_stack, vector<long long int> final_nodes, bulk::queue<long long int, int[], long long int, long long int> &send_work) {
-    if (!work_stack.empty())
-        for (long long int i = 0; i < work_stack.at(0).visited.size(); i ++) {
-            world.log("node %d is visited? %d", i, work_stack.at(0).visited.at(i));
-        }
+void send_works(bulk::world &world, long long int p, long long int pid, vector<work> &work_stack, bulk::queue<long long int, int[], long long int> &send_work, map<long long int, int> shared_works) {
+    // if (!work_stack.empty())
+    //     for (long long int i = 0; i < work_stack.at(0).visited.size(); i ++) {
+    //         world.log("node %d is visited? %d", i, work_stack.at(0).visited.at(i));
+    //     }
     // auto send_work = bulk::queue<long long int, int[], long long int, long long int>(world);
 
-    for (long long int i = 0; i < p; i ++) {
-        if (i != pid) {
-            for (auto work : work_stack) {
-                // for (auto node : final_nodes) {
-                    send_work(i).send(work.w, work.visited, work.count, work.N);
-                    world.log("processor %d sends starting node = %d, count = %d, path length = %d", pid, work.w, work.count, work.N);
-                // }
-            }
+    // for (long long int i = 0; i < p; i ++) {
+    //     if (i != pid) {
+    //         for (auto work : work_stack) {
+    //             // for (auto node : final_nodes) {
+    //                 send_work(i).send(work.w, work.visited, work.count, work.N);
+    //                 world.log("processor %d sends starting node = %d, count = %d, path length = %d", pid, work.w, work.count, work.N);
+    //             // }
+    //         }
+    //     }
+    // }
+    for (std::pair<long long int, int> shared_work : shared_works){
+        long long int target_processor = shared_work.first;
+        long long int offloaded_workload = shared_work.second;
+        for (long long int i = 0; i < offloaded_workload; i ++) {
+            work work_to_send = work_stack.at(work_stack.size() - 1);
+            work_stack.pop_back();
+            send_work(target_processor).send(work_to_send.v, work_to_send.visited, work_to_send.cur_path_length);
         }
     }
+                
 }
 
-map<long long int, int> redistribute_work(bulk::world &world, vector <int> work_stack_lengths, long long int pid, long long int p) {
+map<long long int, int> redistribute_work(bulk::world &world, vector <long long int> work_stack_lengths, long long int pid, long long int p) {
     /*
         Input ::
         world, same as always
@@ -210,57 +246,128 @@ int main(int argc, char* argv[]) {
     env.spawn(p, [&n, &p, &flops, &N, &v, &A](auto& world) {
         // init local processors
         auto pid = world.rank(); // local processor ID
-        long long int flop = 0;
-        long long int count = 0;
-        int time_since_last_sync = SYNC_TIME - 2;
-        long long int cur_N = 1;
+        long long int flop       = 0;
+        long long int count      = 0;
+        long long int SYNC_TIME  = 0;
+        long long int time_since_last_sync = 0;
+        long long int path_length_so_far = 0;
         vector<work> work_stack;
         vector<int> visited(n, false);
+        vector<long long int> final_nodes;
         vector<long long int> walk;
         walk.push_back(v);
-        work_stack.push_back(work(v, visited, count, cur_N));
-        auto send_work = bulk::queue<long long int, int[], long long int, long long int>(world);
-        auto send_done = bulk::queue<int>(world);
+
+        work_stack.push_back(work(v, visited, path_length_so_far));
+        
+        auto send_work              = bulk::queue<long long int, int[], long long int>(world);
+        // auto send_done              = bulk::queue<int>(world);final_nodes
+        auto send_work_stack_length = bulk::queue<long long int, int>(world);
+        // auto send_work              = bulk::queue<int>(world);
 
 
         int done = false;
         while (!done) {
         // for (int i = 0; i < 2; i ++) {
-            for (auto [v, visited, count, cur_N] : send_work) {
-                work_stack.push_back(work(v, visited, count, cur_N));
-                world.log("processor %d received starting node = %d, count = %d, cur_N = %d", pid, v, count, cur_N);
-            }
+            // for (auto [v, visited, count, cur_N] : send_work) {
+            //     work_stack.push_back(work(v, visited, count, cur_N));
+            //     world.log("processor %d received starting node = %d, count = %d, cur_N = %d", pid, v, count, cur_N);
+            // }
 
             if (work_stack.empty()) {
                 done = true;
-            } else {
+            } 
+            else {
                 work work = work_stack.at(work_stack.size() - 1);
                 work_stack.pop_back();
-                vector<long long int> final_nodes;
-                world.log("processor %d does work starting node = %d, count = %d, path length = %d", pid, work.w, work.count, work.N);
+                // world.log("processor %d does work starting node = %d, count = %d, path length = %d", pid, work.w, count, work.N);
 
 
-                saw(world, n, N, work.w, A, work.N - 1, work.visited, walk, work.count, p, pid, work_stack, final_nodes, work.N);
+                saw(world, n, N, work.v, A, work.cur_path_length, work.visited, count, p, pid, work_stack);
 
-                if (work.N < N) {
-                    // for (auto node : final_nodes) {
-                    //     work_stack.push_back({node, work.visited, work.count, cur_N + 1});
-                    // }
-                    world.log("still got work to do, cur_N = %d, N = %d, found %d paths so far", work.N, N, work.count);
-                    send_works(world, p, pid, work_stack, final_nodes, send_work);
-                }
+                // if (work.N < N) {
+                //     // for (auto node : final_nodes) {
+                //     //     work_stack.push_back({node, work.visited, work.count, cur_N + 1});
+                //     // }
+                //     world.log("still got work to do, cur_N = %d, N = %d, found %d paths so far", work.N, N, work.count);
+                //     send_works(world, p, pid, work_stack, send_work);
+                // }
             }
+
+            
             // get work
+            // In order to redistribute the work, we share the lengths of our work stacks with everybody, and unpack it later. 
+            //Syncing method below. 
+            if (time_since_last_sync == SYNC_TIME) {
+                //First we share how much work we have 
+                send_work_stack_lengths(world, p, pid, work_stack.size(), send_work_stack_length);
+                // send_dones(world, p, pid, done, send_done);
+                world.sync();
+                //Then we receive the work.  
 
-            send_dones(world, p, pid, done, send_done);
-            world.sync();
-
-            for (auto remote_done : send_done) {
-                if (!remote_done) {
-                    done = false;
+                vector<long long int> work_stack_lengths(p, 0);
+                for (auto [remote_pid, remote_work_stack_length] : send_work_stack_length) {
+                    work_stack_lengths[remote_pid]  = remote_work_stack_length;
                 }
+                // We check if we are done
+                done = true;
+                for (long long int i = 0 ; i < work_stack_lengths.size(); i ++){
+                    if (work_stack_lengths[i] != 0 ){
+                        done  = false;
+                    }
+                }
+
+                //Now, we calculate how the work should be redistributed.
+                map<long long int, int> shared_work = redistribute_work(world, work_stack_lengths, pid, p);
+                // Then, we share the work we need to share
+                send_works(world, p, pid, work_stack, send_work, shared_work);
+                world.sync();
+                //Finally we add the work we received  to our workstack 
+                 
+                for (auto [v, visited, cur_N] : send_work) {
+                    work_stack.push_back(work(v, visited, cur_N));
+                    SYNC_TIME = work_stack.size();
+                    time_since_last_sync = 0; 
+                    // world.log("processor %d received starting node = %d, count = %d, cur_N = %d", pid, v, count, cur_N);
+                }
+                //we now should have workstacks of comparable size for each processor.
             }
-        }
+
+
+
+            
+                //Then we work out wether we're done and if not, how we should redistribute the work
+                
+                //Then we redistribute the work.
+
+                //we should start working on the stack again, but first we are going to reset our time_since_last_sync and our  
+
+
+                /* below is notepad
+                While !done:
+                    For (i = 0 ; i<synctime ; i++ ){
+                        saw one step of the workstack 
+                    }
+
+                    Share workstack lengths
+                    sync
+                    find done's 
+                    Redistribute work
+                    sync
+                    We know have a new workstack. 
+                    synctime = workstack.size()
+
+                */
+
+            }
+
+            // for (auto remote_done : send_done) {
+            //     if (!remote_done) {
+            //         done = false;
+            //     }
+            // }
+
+
+        // }
 
         flops[pid] = flop;
 
