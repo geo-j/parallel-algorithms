bcdabf56cc5a84cd9fe40b513c927aaba98bf9dd
geo-j <g.juglan@tutanota.com>
2021-12-07 12:24:25 +0100

    redistribute work implemented

--- Ass2/saw.cpp
+++ Ass2/saw.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <fstream>
 // #include "funcs.hpp"
+#include <numeric>
 #include <vector>
 #include <map>
 #include <algorithm>
@@ -64,6 +65,7 @@ void saw(bulk::world &world, long long int n, long long int N, long long int v, 
             visited[v] = true;
             for (long int w = 0; w < n; w ++) {
                 if (A[v][w]) {
+                    // work_stack.push_back(work(w, vector<int>(visited), count, cur_N ));
                     walk.push_back(w);
                     saw(world, n, N, w, A, i + 1, visited, walk, count, p, pid, work_stack, final_nodes, cur_N);
                     walk.pop_back();
@@ -102,6 +104,77 @@ void send_works(bulk::world &world, long long int p, long long int pid, vector<w
         }
     }
 }
+
+map<long long int, int> redistribute_work(bulk::world &world, vector <int> work_stack_lengths, long long int pid, long long int p) {
+    /*
+        Input ::
+        world, same as always
+        work_stack_lengths: for every processor we have the length of their work stack
+        pid:: which processor are we
+        p:: how many processors are there
+
+        Output::
+        A map from int to int, representing how much work this processor should send to each processor (so if the map is [(1,100), (4,20)] we'll send 100 work to processor 1 and 20 work to processor 4)
+    */
+
+    long long int total_workload = accumulate(work_stack_lengths.begin(), work_stack_lengths.end(), 0);
+    // long long int ideal_fair_workload = static_cast<long long int>(total_workload / p);
+    //The best workload is sorta cyclically distributed
+    vector<long long int> fair_work_stack_lengths(p, 0);
+    
+    // Our ideal workload is one where each processor has about the same amount of work, 
+    // This distribution is inspired by a cyclic work distribution, which we use to not deal with rounding errors. 
+    long long int current_pid = 0;
+    for (int i = 0; i < total_workload; i ++) {
+        fair_work_stack_lengths[current_pid] ++;
+        current_pid = (current_pid + 1) % p; 
+    } 
+
+    map<long long int, int> sharing_map;
+    //the following loop will go trough how the work should be offloaded by everyone 
+    //when we are at our own processor and we see we should offload work, 
+    // we'll keep track of it and actually put it in the map
+        // for (long long int current_offloader = 0; current_offloader < pid + 1; current_offloader ++) {
+    for (long long int current_offloader = 0; current_offloader < pid + 1; current_offloader ++) {
+        long long int current_receiver = 0;
+        while (work_stack_lengths[current_offloader] - fair_work_stack_lengths[current_offloader] > 0) {    // as long as we still have work to offload
+            long long int receiver_needs = work_stack_lengths[current_receiver] - fair_work_stack_lengths[current_receiver];
+            if (receiver_needs < 0) {    // for anyone who still needs work
+                long long int can_offload = work_stack_lengths[current_offloader] - fair_work_stack_lengths[current_offloader];
+                long long int offloaded;
+                if (abs(receiver_needs) > can_offload) {
+                    work_stack_lengths[current_offloader] -= can_offload;
+                    work_stack_lengths[current_receiver]  += can_offload;
+                    offloaded = can_offload;
+                }
+                else {
+                    //keep in mind that receiver needs is negative
+                    work_stack_lengths[current_offloader] += receiver_needs;
+                    work_stack_lengths[current_receiver]  -= receiver_needs;
+                    offloaded = -receiver_needs;
+
+                }
+                if (current_offloader == pid) {
+                    sharing_map.insert(std::pair(current_receiver, offloaded));
+                }
+
+                // work_stack_lengths[current_offloader] -= receiver_needs;
+                // work_stack_lengths[current_receiver]  += receiver_needs;
+                // // Now the receiver should have all the work it needs, but we might have overshot a bit, in which case we need to go back 
+                // long long int overshoot = work_stack_lengths[current_offloader] - fair_work_stack_lengths[current_offloader]; 
+                // if (overshoot < 0) {
+                //     work_stack_lengths[current_offloader] += overshoot;
+                //     work_stack_lengths[current_receiver]  -= overshoot;
+            }
+            current_receiver ++;
+        }
+    }
+
+    return sharing_map;
+}
+    
+
+
 
 int main(int argc, char* argv[]) {
     bulk::thread::environment env;
@@ -173,7 +246,7 @@ int main(int argc, char* argv[]) {
                     // for (auto node : final_nodes) {
                     //     work_stack.push_back({node, work.visited, work.count, cur_N + 1});
                     // }
-                    world.log("YOOOOOOOOOO still got work to do, cur_N = %d, N = %d, found %d paths so far", work.N, N, work.count);
+                    world.log("still got work to do, cur_N = %d, N = %d, found %d paths so far", work.N, N, work.count);
                     send_works(world, p, pid, work_stack, final_nodes, send_work);
                 }
             }
